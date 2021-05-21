1d0fcefddaaee8e6345e81f3a649aec14ab9ef90
Camilla <camilla.lima000@gmail.com>
2021-05-21 16:24:43 +0100

    Corrected spelling mistake

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,7 +7,7 @@ describe "fizzbuzz" do
   it 'returns "buzz" when divisible by 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" when dvisible by 3 and 5' do
+  it 'returns "fizzbuzz" when divisible by 3 and 5' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
   it 'returns number when not divisible by 3 nor 5' do
